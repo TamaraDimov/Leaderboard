e19c7ac6d0d19cfc8ab22d47af388ada1ebc0da1
TamaraDimov <tamemojsoska@gmail.com>
2023-02-08 12:05:41 +0100

    implementation of Project requirements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './style.css';
+import score from './modules/submit.js';
+import { showScore, getScore } from './modules/score.js';
+
+const inputName = document.querySelector('#nameInput');
+const inputScore = document.querySelector('#scoreInput');
+const submitBtn = document.querySelector('#submit-btn');
+const refreshBtn = document.getElementById('refresh-btn');
+
+window.addEventListener('load', () => {
+  showScore();
+});
+
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  score();
+  inputName.value = '';
+  inputScore.value = '';
+});
+
+refreshBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  getScore();
+  showScore();
+});
---
+++ src/modules/submit.js
@@ -0,0 +1,20 @@
+const score = async () => {
+  const response = await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/k9cpJwUktCh3XoogocI2/scores/',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        user: document.querySelector('#nameInput').value,
+        score: document.querySelector('#scoreInput').value,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    }
+  );
+  const addedScore = await response.json();
+  return addedScore;
+  // console.log(addedScore);
+};
+
+export default score;
