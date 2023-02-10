5c9be3aa69066fc963c4bf665b316cb69a792aaf
TamaraDimov <tamemojsoska@gmail.com>
2023-02-10 12:33:37 +0100

    add style on the page

--- src/modules/score.js
+++ src/modules/score.js
@@ -1,6 +1,6 @@
 export const getScore = async () => {
   const response = await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/k9cpJwUktCh3XoogocI2/scores/',
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/k9cpJwUktCh3XoogocI2/scores/'
   );
   const endScore = await response.json();
   return endScore.result;
@@ -13,10 +13,12 @@ export const showScore = async () => {
   table.innerHTML = '';
   data.forEach((value) => {
     const renderData = `
-    <div class="table-scores">
-    <div>${value.user}</div>
-    <div>${value.score}</div>
-    </div>`;
+    <table class="table-scores">
+    <tr id="row">
+    <td class="user">${value.user}</td>
+    <td class="score">${value.score}</td>
+    </tr>
+    </table>`;
     table.insertAdjacentHTML('beforeend', renderData);
   });
 };
